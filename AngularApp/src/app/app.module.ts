f0dcd4d1492da0122c7b6dfebbd99b22467f1363
ShohruhUzDev <shohruh7464784@mail.ru>
2022-09-26 14:59:24 +0500

    add new solution

---
+++ AngularApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { LessonComponent } from './lesson/lesson.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LessonsComponent,
+    LessonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularApp/src/app/lesson.ts
@@ -0,0 +1,7 @@
+export class LessonsComponent{
+  id:number|undefined;
+  title:string |undefined;
+  price:number|undefined;
+  duration:number|undefined;
+  intakeDeadline:Date|undefined
+}
